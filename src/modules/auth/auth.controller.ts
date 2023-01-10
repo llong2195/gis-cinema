223125e6d9213893640be19679dc1b17a59b23a3
llong2195 <nduylong9501@gmail.com>
2023-01-10 21:10:39 +0700

    hanlde throttler guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RegisterRequestDto } from './dto/register-request.dto';
 import { AuthUser } from 'src/decorators/auth.user.decorator';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Throttle } from '@nestjs/throttler';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
 @ApiTags('v1/auth')
 @Controller('v1/auth')
@@ -19,7 +19,8 @@ export class AuthController {
 
     // @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
-    @Throttle(3, 2)
+    @UseGuards(ThrottlerGuard)
+    @Throttle(10, 2)
     @Post('/login')
     async login(@Body() request: LoginRequestDto): Promise<BaseResponseDto<any>> {
         const data = await this.authService.login(request);
